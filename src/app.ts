dbed7b490ae6c3ce377cee7c9c10f0ed4e7c114c
David Mears <clarinet.david@gmail.com>
2026-08-17 17:27:42 +0100

    Implement prevalences endpoint

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import { errorHandler } from './middlewares/errorHandler.ts';
 const latestModelVersion = "2026.05.08";
 
 const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+const adminLevels = ["0", "1", "2"];
 
 // Request grout to get bounding boxes for admin0.
 // TODO: fall back when grout is down
@@ -163,9 +164,17 @@ const requestableSurveyProperties = [
 // can duckdb streaming help?
 
 app.get('/surveys', async (req: Request, res: Response) => {
+  // TODO: type queryParams for this endpoint and validate all params (don't allow unrecognised params)
   const queryParams = req.query as Record<string, string | undefined>;
   if (!queryParams.data_release) {
     res.status(400).send({ error: "Missing required query parameter: data_release" });
+    return;
+  }
+
+  // Security: Validate that the above is a filepath within the expected data directory,
+  // by comparing it against a list of the actual survey data releases in the data/stave directory.
+  if (!dataVersions.includes(queryParams.data_release)) {
+    res.status(400).send({ error: `Invalid data release requested: ${queryParams.data_release}` });
     return;
   }
 
@@ -198,13 +207,6 @@ app.get('/surveys', async (req: Request, res: Response) => {
 
   const surveyDataParquet = `data/stave/${queryParams.data_release}/survey_data.parquet`;
 
-  // Security: Validate that the above is a filepath within the expected data directory,
-  // by comparing it against a list of the actual survey data releases in the data/stave directory.
-  if (!dataVersions.includes(queryParams.data_release)) {
-    res.status(400).send({ error: `Invalid data release requested: ${queryParams.data_release}` });
-    return;
-  }
-
   const properties = queryParams.properties?.split(',') ?? [];
   if (properties.length === 0) {
     res.status(400).send({ error: "At least one property must be requested." });
@@ -214,15 +216,15 @@ app.get('/surveys', async (req: Request, res: Response) => {
   const columnsInData = await connection.runAndReadAll(`SELECT * FROM '${surveyDataParquet}' LIMIT 1`);
   const availableColumns = columnsInData.columnNames(); // Conceivably will vary by data release if schema changes
 
-  if (!properties.every(p => requestableSurveyProperties.includes(p) && availableColumns.includes(p))) {
-    const invalid = properties.find(p => !requestableSurveyProperties.includes(p) || !availableColumns.includes(p));
+  const invalid = properties.find(p => !requestableSurveyProperties.includes(p) || !availableColumns.includes(p));
+  if (invalid) {
     res.status(400).send({ error: `Invalid property requested: ${invalid}` });
     return;
   }
 
   const selectableParams = ["admin0", "survey_id", "date_from", "date_to", "gene", "mutation"]
     .filter(param => !!queryParams[param]);
-  const tableName = `c`;
+  const tableName = "c";
   const whereClauses = ["1 = 1"]; // Start with a dummy condition to simplify appending AND clauses
   selectableParams.forEach((param) => {
     if (param === "admin0") {
@@ -248,7 +250,7 @@ app.get('/surveys', async (req: Request, res: Response) => {
     whereClauses.push(`${tableName}.${column} ${equality} $${param}`);
   });
 
-  const columns = properties.map(p => `c.${p}`).join(", ");
+  const columns = properties.map(p => `${tableName}.${p}`).join(", ");
   const statement = await connection.prepare(
     `SELECT ${columns} FROM '${surveyDataParquet}' ${tableName} WHERE ${whereClauses.join(' AND ')}`
   )
@@ -262,18 +264,153 @@ app.get('/surveys', async (req: Request, res: Response) => {
     date_to: VARCHAR,
     gene: VARCHAR,
     mutation: VARCHAR,
-  },
-);
+  });
   const result = await statement.runAndReadAll();
   const rows = result.getRowObjectsJson();
 
   res.send(rows);
 });
 
-app.get('/prevalences', (req: Request, res: Response) => {
-  const admin_level = req.query['admin_level'];
+const requestablePrevalenceProperties = [
+  "variant",
+  "gene",
+  "mutation",
+  "admin0",
+  "admin1",
+  "admin2",
+  "date",
+  "mean",
+  "median",
+  "SD",
+  "lower_95",
+  "upper_95",
+  "exceedance_1",
+  "exceedance_2",
+  "exceedance_5",
+  "exceedance_10",
+  "no_of_informing_surveys",
+  "nearest_survey_by_date" // gives a survey_id
+]
 
-  res.send({admin_level_query_param: admin_level});
+app.get('/prevalences', async (req: Request, res: Response) => {
+  // TODO: type queryParams for this endpoint and validate all params (don't allow unrecognised params)
+  const queryParams = req.query as Record<string, string | undefined>;
+
+  const modelVersion = queryParams.model_release as string;
+  if (!modelVersion) {
+    res.status(400).send({ error: "Missing required query parameter: model_release" });
+    return;
+  }
+
+  // Security: Validate that the above is a filepath within the expected data directory,
+  // by comparing it against a list of the actual survey data releases in the data/stave directory.
+  if (!modelVersions.includes(modelVersion)) {
+    res.status(400).send({ error: `Invalid model release requested: ${modelVersion}` });
+    return;
+  }
+
+  ["date", "date_from", "date_to"].forEach(param => {
+    if (queryParams[param] && !dateRegex.test(queryParams[param]!)) {
+      res.status(400).send({ error: `Invalid date format for parameter '${param}'. Expected YYYY-MM-DD.` });
+      return;
+    }
+  });
+
+  // Check the date is the first of a month
+  if (queryParams.date && new Date(queryParams.date).getDate() !== 1) {
+    res.status(400).send({ error: "Invalid `date` parameter. The date must be the first of a month." });
+    return;
+  }
+
+  if (queryParams.admin0 && !/^[A-Z]{3}$/.test(queryParams.admin0)) {
+    res.status(400).send({ error: "Invalid ISO code for parameter 'admin0'. Expected a 3-letter uppercase ISO code." });
+    return;
+  }
+
+  if (queryParams.date_from && !queryParams.date_to) {
+    res.status(400).send({ error: "Missing required parameter 'date_to' when 'date_from' is specified." });
+    return;
+  }
+
+  if (queryParams.date_to && !queryParams.date_from) {
+    res.status(400).send({ error: "Missing required parameter 'date_from' when 'date_to' is specified." });
+    return;
+  }
+
+  if (queryParams.date_from && queryParams.date_to && new Date(queryParams.date_from) > new Date(queryParams.date_to)) {
+    res.status(400).send({ error: "'date_from' cannot be later than 'date_to'." });
+    return;
+  }
+
+  // Client may request results at any of the available levels of granularity.
+  const adminLevel = queryParams.admin_level;
+  if (!adminLevel) {
+    res.status(400).send({ error: "Missing required query parameter: admin_level" });
+    return;
+  }
+  if (!adminLevels.includes(adminLevel)) {
+    res.status(400).send({ error: `Invalid admin level requested: ${adminLevel}` });
+    return;
+  }
+
+  const prevalencesParquet = `data/model/${modelVersion}/admin${adminLevel}.parquet`;
+
+  const columnsInData = await connection.runAndReadAll(`SELECT * FROM '${prevalencesParquet}' LIMIT 1`);
+  const availableColumns = columnsInData.columnNames(); // Conceivably will vary by model release if schema changes
+
+  adminLevels.forEach((level) => {
+    if (queryParams[`admin${level}`] && adminLevel < level) {
+      res.status(400).send({ error: "You cannot request results at a less granular level than that of the containing region." })
+    }
+  })
+
+  const properties = queryParams.properties?.split(',').filter(p => !!p) ?? [];
+  if (properties.length === 0) {
+    res.status(400).send({ error: "At least one property must be requested." });
+    return;
+  }
+
+  const invalid = properties.find(p => !requestablePrevalenceProperties.includes(p) || !availableColumns.includes(p));
+  if (invalid) {
+    res.status(400).send({ error: `Invalid property requested: ${invalid}` });
+    return;
+  }
+
+  const selectableParams = ["admin0", "admin1", "admin2", "gene", "mutation", "date", "date_from", "date_to"]
+    .filter(param => !!queryParams[param]);
+  const tableName = "p";
+  const whereClauses = ["1 = 1"]; // Start with a dummy condition to simplify appending AND clauses
+  selectableParams.forEach((param) => {
+    const column = ["date_from", "date_to"].includes(param) ? "date" : param;
+    const equality = param === "date_from" ? ">=" : param === "date_to" ? "<=" : "=";
+
+    whereClauses.push(`${tableName}.${column} ${equality} $${param}`);
+    return;
+  });
+
+  const columns = properties.map(p => `${tableName}.${p}`).join(", ");
+
+  const statement = await connection.prepare(
+    `SELECT ${columns} FROM '${prevalencesParquet}' ${tableName} WHERE ${whereClauses.join(' AND ')}`
+  )
+  const bindings = selectableParams.reduce((acc, param) => {
+    acc[param] = queryParams[param] ?? null;
+    return acc;
+  }, {} as Record<string, string | null>);
+  statement.bind(bindings, {
+    admin0: VARCHAR,
+    admin1: VARCHAR,
+    admin2: VARCHAR,
+    gene: VARCHAR,
+    mutation: VARCHAR,
+    date: VARCHAR,
+    date_from: VARCHAR,
+    date_to: VARCHAR,
+  });
+  const result = await statement.runAndReadAll();
+  const rows = result.getRowObjectsJson();
+
+  res.send(rows);
 });
 
 app.use(errorHandler);
