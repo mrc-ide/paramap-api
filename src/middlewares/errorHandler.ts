d551405d49d80d84dc8627e965f530e47d2bc4b1
David Mears <clarinet.david@gmail.com>
2026-08-25 15:52:34 +0100

    Move error handler call to correct place

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -8,8 +8,10 @@ export const errorHandler = (
   err: AppError,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
+  // TODO: Add logging to an external service here
   console.error(err);
   res.status(err.status || 500).json({
     message: err.message || 'Internal Server Error',
