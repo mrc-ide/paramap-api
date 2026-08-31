82ff7c957592500d47421316131818e10ae8fca6
David Mears <clarinet.david@gmail.com>
2026-08-31 15:54:12 +0100

    Revert "Remove tests"
    This reverts commit 30505c19f5ce1082684bea8766c94ca8516df319.

--- tests/unit/validators.test.ts
+++ tests/unit/validators.test.ts
@@ -65,7 +65,7 @@ describe('validateRequestedProperties', () => {
     const { res } = mockReqRes({});
 
     expect(
-      validateRequestedProperties({ properties: '' }, ['admin1', 'median'], dbTypes, res)
+      validateRequestedProperties([], ['admin1', 'median'], dbTypes, res)
     ).toBe(false);
     expect(res.status).toHaveBeenCalledWith(400);
   });
@@ -74,7 +74,7 @@ describe('validateRequestedProperties', () => {
     const { res, } = mockReqRes({});
 
     expect(
-      validateRequestedProperties({ properties: 'admin1,median' }, ['median'], dbTypes, res)
+      validateRequestedProperties(['admin1', 'median'], ['median'], dbTypes, res)
     ).toBe(false);
     expect(res.send).toHaveBeenCalledWith({ error: 'Invalid property requested: admin1' });
     expect(res.status).toHaveBeenCalledWith(400);
@@ -84,7 +84,7 @@ describe('validateRequestedProperties', () => {
     const { res, } = mockReqRes({});
 
     expect(
-      validateRequestedProperties({ properties: 'median' }, ['median'], {}, res)
+      validateRequestedProperties(['median'], ['median'], {}, res)
     ).toBe(false);
     expect(res.send).toHaveBeenCalledWith({ error: 'Invalid property requested: median' });
     expect(res.status).toHaveBeenCalledWith(400);
@@ -94,7 +94,7 @@ describe('validateRequestedProperties', () => {
     const { res } = mockReqRes({});
 
     expect(
-      validateRequestedProperties({ properties: 'admin1,median' }, ['admin1', 'median'], dbTypes, res)
+      validateRequestedProperties(['admin1', 'median'], ['admin1', 'median'], dbTypes, res)
     ).toBe(true);
   });
 });
